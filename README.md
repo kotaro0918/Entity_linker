872b93c46e5c46843f49c0667be801616771ab49
kotaro <big.i.land.k@gmail.com>
2023-05-31 16:43:48 +0900

    modified readme.md,entity_linker.py

--- README.md
+++ README.md
@@ -1,22 +1,66 @@
 # VSCode を用いたエンティティリンキング
 
 vscode を用いてテキストの中の都道府県名と市町村名にエンティティリンキングを行う
-### 使い方
+
+
+
+
+
+
+
+##　使い方
 
 1. 作成したレポジトリをローカル環境（PC）上に clone する。
-1. ローカルのレポジトリで vscode を開く
+
+2.  clone したローカル環境下に　.devcontainer/devcontainer.envのファイルを作り
+```
+OPENAI_API_KEY=自分のAPI Key
+```
+を書き込む 
+
+ 3. .devcontainer/devcontainer.jsonに
+```
+"runArgs": ["--env-file", ".devcontainer/devcontainer.env"]
+```
+を書き込む   
+
+4. このままだと.devcontainer/devcontainer.env もpushされてしまうので.gitignoreに
+```
+*.env
+```
+を書き込む
+
+### 注意
+.gitignore に書き込む前に .devcontainer/devcontainer.envをadd するとignoreされないので注意
+
+
+5. ローカルのレポジトリで vscode を開く
 
 ```
 code ディレクトリ
 ```
 
-3. vscode 左下の `><` こんなアイコンをクリックして `Reopen in container` を選択して、python が実行可能なコンテナ内で vscode を開く
+6. vscode 左下の `><` こんなアイコンをクリックして `Reopen in container` を選択して、python が実行可能なコンテナ内で vscode を開く
 
-4. vscode でentity_linker.py を実行した後、ターミナル内に好きな文章を入力する
+7. vscode でentity_linker.py を実行した後、ターミナル内に好きな文章を入力する
 
-5. 結果が
+8. 結果が
 ```
-埼玉県-都道府県-埼玉県 https://ja.wikipedia.org/wiki/埼玉県        三郷市-市-三郷市 https://ja.wikipedia.org/wiki/三郷市
+三郷市-市
+三郷市
+https://ja.wikipedia.org/wiki/三郷市
+
+埼玉県-都道府県
+埼玉県
+https://ja.wikipedia.org/wiki/埼玉県
+
+東京都-都道府県
+東京都
+https://ja.wikipedia.org/wiki/東京都
+
+千葉県-都道府県
+千葉県
+https://ja.wikipedia.org/wiki/千葉県
 ```
 のように出力される
 
--- entity_linker.py
+++ entity_linker.py
@@ -2,11 +2,12 @@ import os
 import openai
 openai.api_key = os.environ["OPENAI_API_KEY"]
 
-text_input = input()
+text_input = input("文章を入力してください")
 response = openai.ChatCompletion.create(
     model="gpt-3.5-turbo",
     messages=[
         {"role": "user", "content": "下の文章から都道府県名や市区町村名に対してエンティティリンキングを行なってください\
+         出力は 単語-属性-固有名詞 wikipediaのリンク  のようにしてください\
             text: 「根室本線は北海道の滝川駅から帯広、釧路を経て根室駅を結ぶJR北海道の路線です。このうち釧路駅から根室駅までの区間は「花咲線」の愛称で呼ばれています。\
             観光シーズンには札幌からのリゾート列車が多数運行されます。キハ283系の車体は、ブルーとグリーンに丹頂鶴の赤を組み合わせ北海道らしさを演出しています.」"},
         {"role": "assistant", "content": "北海道-都道府県-北海道 https://ja.wikipedia.org/wiki/北海道\
@@ -14,8 +15,24 @@ response = openai.ChatCompletion.create(
         釧路-市-釧路市 https://ja.wikipedia.org/wiki/釧路市\
         根室-市-根室市 https://ja.wikipedia.org/wiki/根室市\
         札幌-市-札幌市 https://ja.wikipedia.org/wiki/札幌市"},
-        {"role": "user", "content": f"{text_input}に対して上のようにエンティティリンキングを行なってください。この際、都道府県名と市町村名のみが対象であることに気をつけてください"}     
+        {"role": "user", "content": f"{text_input}に対して上のようにエンティティリンキングを行なってください。この際、属性が都道府県名と市町村名であるもののみ対象であることに気をつけてください\
+         出力結果は\
+        帯広-市-帯広市 https://ja.wikipedia.org/wiki/帯広市\
+        釧路-市-釧路市 https://ja.wikipedia.org/wiki/釧路市\
+        根室-市-根室市 https://ja.wikipedia.org/wiki/根室市\
+        札幌-市-札幌市 https://ja.wikipedia.org/wiki/札幌市    のように読みやすいように改行してください"}     
     ],
+    temperature=1
 )
+
+result = response.choices[0]["message"]["content"].strip()
+response = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    messages=[
+        {"role": "user", "content": f"{result}に対して読みやすいように改行をおこなって出力してください"} 
+    ],
+    temperature=1
+)
+
 print(response.choices[0]["message"]["content"].strip())
 
